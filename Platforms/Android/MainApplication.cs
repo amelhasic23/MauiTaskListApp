7ab6503a2d6c2578ee23534b5f6639a8da8a1169
Amel Hasić <amelhasic23@outlook.com>
2025-03-03 09:38:23 +0100

    Commit

---
+++ Platforms/Android/MainApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTaskListApp;
+
+using Android.App;
+using Android.Runtime;
+
+[Application]
+public class MainApplication(IntPtr handle, JniHandleOwnership ownership) : MauiApplication(handle, ownership)
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
